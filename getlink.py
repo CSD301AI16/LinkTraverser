39556b1d71ef53a7f951a36789874e23c40e0365
The Khoi Nguyen <53959952+nguyenthekhoig7@users.noreply.github.com>
2022-06-14 23:59:07 +0700

    Update getlink.py
    Removed the hrefs reference to the original link from the href_list

--- getlink.py
+++ getlink.py
@@ -1,21 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class LinkTraverser():
     rootURL: str
-    max: int  # default 200
+    max: int                # default 200
 
     def __init__(self, rootURL: str, max = 200):
-        self.rootURL = rootURL
+        self.rootURL = rootURL.rstrip('/')
         try:
-            page = requests.get(rootURL)
+            page = requests.get(self.rootURL)
             print("Valid URL. A Traverser object created.")
         except requests.exceptions.RequestException as err:
             print("Invalid URL. Cannot reach page.")
-            raise err  # if not raising error here. An object with invalid URL will eventually cause exception later
+            raise err   # if not raising error here. An object with invalid URL will eventually cause exception later
 
         self.max = max
+
 
     def get_href_list(self):
         page = requests.get(self.rootURL)
@@ -25,17 +25,30 @@ class LinkTraverser():
             href = a.get("href")
             if href is None:
                 continue
-            if not ('.com' in href or '.vn' in href):  ## href is a subfolder in the webpage
-                href = self.rootURL + href
+            if not ('.com' in href or '.vn' in href):           ## href is a subfolder in the webpage
+                href = self.rootURL + '/' + href
+            href = href.rstrip('/')
+            if href == self.rootURL:
+                continue
             href_list.append(href)
             if len(href_list) >= self.max:
                 break
         return href_list
 
 
-url = "https://fptshop.com.vn/"
+    
+##### MAIN #####
 
-travser1 = LinkTraverser(rootURL = url, max = 23)
-list = travser1.get_href_list()
-if list is not None:
-    print(len(list))
+# url = "https://fptshop.com.vn/"
+
+# travser1 = LinkTraverser(rootURL = url, max = 23)
+# list = travser1.get_href_list()
+# if list is not None:
+#     print(len(list))
+
+
+# travser2 = LinkTraverser(rootURL = url)
+# list = travser2.get_href_list()
+# if list is not None:
+#     print(len(list))        
+#     print(list)
