c3dba7fa21473e805c201e069bdf6e184cc4badd
trinhkiet0105 <76981747+trinhkiet0105@users.noreply.github.com>
2022-06-15 15:05:15 +0700

    update graph

--- Graph.py
+++ Graph.py
@@ -8,34 +8,25 @@ class Graph(object):
 
     def __init__(self, link: str, ibL=[], obL=[]):
         self.rootNode = Node(link)
-        self.rootNode.inboundList = []
-        self.rootNode.outboundList = []
-        for iblink in ibL:
-            if not self.getNode(iblink):
-                curr = Node(iblink, obL=[self.rootNode])
-                self.rootNode.insertInbound(curr)
-                self.insertNode(curr)
-        for oblink in obL:
-            if not self.getNode(oblink):
-                curr = Node(oblink, ibL=[self.rootNode])
-                self.rootNode.insertOutbound(curr)
-                self.insertNode(curr)
-        self.summaryLinkList[link] = self.rootNode
-        return
+        self.rootNode = self.insertNode(self.rootNode, ibL, obL)
+        return None
 
     def insertNode(self, node: Node, ibL=[], obL=[]):
-        self.summaryLinkList[node.link] = node
         for iblink in ibL:
             if not self.getNode(iblink):
-                curr = Node(iblink, obL=[self.rootNode])
+                curr = Node(iblink, obL=[node])
                 self.rootNode.insertInbound(curr)
                 self.insertNode(curr)
         for oblink in obL:
             if not self.getNode(oblink):
-                curr = Node(oblink, ibL=[self.rootNode])
+                curr = Node(oblink, ibL=[node])
                 self.rootNode.insertOutbound(curr)
                 self.insertNode(curr)
+        self.summaryLinkList[node.link] = node
         return node
+
+    def insertNodeByLink(self, link: str, ibL=[], obL=[]):
+        return self.insertNode(Node(link), ibL, obL)
 
     def getRoot(self):
         return self.rootNode
@@ -65,7 +56,6 @@ class Graph(object):
                     queue.append(i)
                     visited.append(i)
         return None
-
 
     def printBFT(self):
         visited = [self.rootNode]
