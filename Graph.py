c345fcc6c6bb306f6b2f3c35f2d39d8fdbe98616
minhkhoa2404 <minhkhoatranhoang244@gmail.com>
2022-06-16 12:04:58 +0700

    add ranking

--- Graph.py
+++ Graph.py
@@ -63,9 +63,45 @@ class Graph(object):
 
         while queue:
             curr = queue.pop(0)
-            print (str(curr), end = " ")
+            print(str(curr), end=" ")
             for i in self.rootNode.outboundList:
                 if i not in visited:
                     queue.append(i)
                     visited.append(i)
         return None
+
+    def _insert_node_into_sorted_list(self, node: Node):
+        i = 0
+        while True:
+            # print('Length of sorted list at iteration {}: {}'.format(i, len(self.sorted_list)))
+            if i == len(self.sorted_list):
+                self.sorted_list.append(node)
+                return
+            if node.getNumberOfInBound() > self.sorted_list[i].getNumberOfInBound():
+                i += 1
+                continue
+            if node.getNumberOfInBound() == self.sorted_list[i].getNumberOfInBound():
+                if node.getNumberOfOutBound() > self.sorted_list[i].getNumberOfOutBound():
+                    i += 1
+                    continue
+                self.sorted_list.insert(i, node)
+                return
+            self.sorted_list.insert(i, node)
+            return
+
+    def sort(self):
+        self.sorted_list = []
+        visited = [self.rootNode]
+        queue = [self.rootNode]
+
+        while queue:
+            curr_node = queue.pop(0)
+            self._insert_node_into_sorted_list(curr_node)
+            for node in curr_node.outboundList:
+                if node not in visited:
+                    queue.append(node)
+                    visited.append(node)
+
+    def print_sorted_list(self):
+        print(*self.sorted_list, sep='\n')
+        print('{} elements in the sorted list'.format(len(self.sorted_list)))
